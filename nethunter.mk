04c3247d7a63e66969bae9b913e54c65c929ccf5
binkybear <binkybear@nethunter.com>
2016-11-12 17:38:01 -0600

    Nethunter location
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- nethunter.mk
+++ nethunter.mk
@@ -26,8 +26,8 @@
 $(call inherit-product, device/htc/flounder/full_flounder.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
-$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/nethunter/config/common_full_phone.mk)
+$(call inherit-product, vendor/nethunter/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := du_flounder
 
